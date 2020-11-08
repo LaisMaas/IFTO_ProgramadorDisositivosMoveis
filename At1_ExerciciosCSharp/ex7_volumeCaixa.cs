f5d6728131fa2a7780b2f9c726430c91c3816341
laisMaas <lais_m_lopes@yahoo.com.br>
2020-11-08 19:46:12 -0300

    ex7 volume caixa

---
+++ At1_ExerciciosCSharp/ex7_volumeCaixa.cs
@@ -0,0 +1,28 @@
+/*Faça um algoritmo que leia os valores de COMPRIMENTO, LARGURA e ALTURA e apresente o valor do volume de uma caixa retangular. Utilize para o cálculo a fórmula VOLUME = COMPRIMENTO * LARGURA * ALTURA.*/
+
+using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double comprimento, largura, altura, volume;
+
+            Console.WriteLine("Informe o comprimento do objeto: ");
+            comprimento = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a largura do objeto: ");
+            largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do objeto: ");
+            altura = double.Parse(Console.ReadLine());
+
+            volume = comprimento * altura * largura;
+
+            Console.WriteLine("O volume deste objeto é: ", volume);
+        }
+    }
+}
+
---
+++ At1_ExerciciosCSharp/ex8_quadradoCubo.cs
@@ -0,0 +1,2 @@
+/*Faça um algoritmo que leia um valor inteiro e apresente os resultados do quadrado e do cubo do valor lido.*/
+
