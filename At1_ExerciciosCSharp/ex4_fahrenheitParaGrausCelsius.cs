19f186db7d41240560226c5fa0b49c10e00aa279
laisMaas <lais_m_lopes@yahoo.com.br>
2020-11-08 17:28:10 -0300

    ex5 calculoCombustível

--- At1_ExerciciosCSharp/ex4_fahrenheitParaGrausCelsius.cs
+++ At1_ExerciciosCSharp/ex4_fahrenheitParaGrausCelsius.cs
@@ -1,3 +1,6 @@
+/*Faça um algoritmo que leia uma temperatura em Fahrenheit e a apresente convertida em graus Celsius. A fórmula de conversão é C = (F – 32) * (5 / 9), na qual F é a temperatura em Fahrenheit e C é a temperatura em Celsius.*/
+
+
 using System;
 
 namespace ConsoleApp4
---
+++ At1_ExerciciosCSharp/ex5_calculoCombustivel.cs
@@ -0,0 +1,28 @@
+/*Faça um algoritmo que calcule a quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE. Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula: LITROS_USADOS = DISTANCIA / 12. O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na viagem.*/
+
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int litrosGastos, distanciaPercorrida, tempoP, velocidadeMedia;
+
+            Console.WriteLine("Informe o tempo de percurso em minutos: ");
+            tempoP = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a velocidade média no percurso: ");
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            distanciaPercorrida = tempoP * velocidadeMedia;
+            litrosGastos = distanciaPercorrida / 12;
+
+            Console.WriteLine("A quantidade de litros de combustível consumido no trajeto foi de: {0}", velocidadeMedia);
+            Console.WriteLine("A quantidade de litros de combustível consumido no trajeto foi de: {0}", tempoP);
+            Console.WriteLine("A quantidade de litros de combustível consumido no trajeto foi de: {0}", distanciaPercorrida);
+            Console.WriteLine("A quantidade de litros de combustível consumido no trajeto foi de: {0}", litrosGastos);
+        }
+    }
+}
